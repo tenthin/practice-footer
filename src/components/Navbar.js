adeb7086be9ee269e7ab89a112a6b1ac5b379807
Tenzin <tenthinten@gmail.com>
2022-01-28 21:10:59 +0530

    space

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <Link to="#" className='menu-bars'>
+                    
+                </Link>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
