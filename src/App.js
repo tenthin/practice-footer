7f5249c8cc7a0d63acc59f8b5bfd89f34adf10ea
Tenzin <tenthinten@gmail.com>
2022-01-29 21:53:39 +0530

    stopping numbers go beyond zero

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
             <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}>
               Increment
             </button>
-            <button className='btn btn-danger mx-3' onClick={() => setCount(count - 1)}>
+            <button className='btn btn-danger mx-3' onClick={() => setCount(count - 1)} disabled={count ===0}>
               Decrement
             </button>
-            <button className='btn btn-secondary mx-3'onClick={() => setCount(0)}>Reset</button>
+            <button className='btn btn-secondary mx-3'onClick={() => setCount(0)} disabled={count ===0}>Reset</button>
           </div>
         </div>
       </div>
