a171ef428df2437f775fd14dbb3c60aa575d708b
Tenzin <tenthinten@gmail.com>
2022-01-29 21:47:07 +0530

    add button for increment decrement reset

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <div className='card text-center my-5'>
         <div className="card-body">
           <h1>Counter App</h1>
+          <div className='my-5'>
+            <h2>Count</h2>
+            <button className='btn btn-success mx-3'>Increment</button>
+            <button className='btn btn-success mx-3'>Decrement</button>
+            <button className='btn btn-success mx-3'>Reset</button>
+          </div>
         </div>
       </div>
     </div>
