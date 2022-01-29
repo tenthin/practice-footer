ac3760bc4c1ed80d6d105eccad66edf3af7fe9bd
Tenzin <tenthinten@gmail.com>
2022-01-29 21:51:35 +0530

    able to increase and decrease numbers

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ function App() {
           <h1>Counter App</h1>
           <div className='my-5'>
             <h2 className='my-5'>{count}</h2>
-            <button className='btn btn-success mx-3'>Increment</button>
-            <button className='btn btn-danger mx-3'>Decrement</button>
-            <button className='btn btn-secondary mx-3'>Reset</button>
+            <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}>
+              Increment
+            </button>
+            <button className='btn btn-danger mx-3' onClick={() => setCount(count - 1)}>
+              Decrement
+            </button>
+            <button className='btn btn-secondary mx-3'onClick={() => setCount(0)}>Reset</button>
           </div>
         </div>
       </div>
